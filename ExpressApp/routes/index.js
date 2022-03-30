31adc8d307960ea889b13c58028a92ece19122ba
a1812047 <vishaldotgarwal@gmail.com>
2022-03-30 15:02:30 +1030

    hey there

--- ExpressApp/routes/index.js
+++ ExpressApp/routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
+const app = require('../app');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(_req, res, _next) {
   res.render('index', { title: 'Express' });
 });
 
+var last_visit = 0;
+router.get('/last.txt', function(_req, res, _next) { 
+  res.send(
+    String(last_visit)
+  );
+  last_visit = new Date();
+});
+
+
+// red, yellow, green, blue.
+const colors = ["red", "yellow", "green", "blue"];
+let i = 0;
+router.get('/color.html', (req, res, next) =>{
+  res.send(`<h1 style = "color: ${colors[i%4]}">${colors[i%4]}</h1>`);
+  i++;
+});
 module.exports = router;
