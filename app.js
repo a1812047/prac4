368d626edf620c57291ab4bb3f049abd4c45553d
a1812047 <vishaldotgarwal@gmail.com>
2022-03-31 12:11:05 +1030

    thi is it

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/last.txt', indexRouter);
-app.use('/color.html', indexRouter);
-app.use('/log.html', indexRouter);
-app.use('/main.html', indexRouter);
-app.use('/first.html', indexRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/last.txt", indexRouter);
+app.use("/color.html", indexRouter);
+app.use("/log.html", indexRouter);
+app.use("/main.html", indexRouter);
+app.use("/first.html", indexRouter);
 module.exports = app;
